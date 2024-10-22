8bb4c71dd915c20d9095db88283aad7065345fcb
Natnael Tsega <42552538+natenaltsega2225@users.noreply.github.com>
2024-10-22 15:25:51 -0500

    attempting

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 const instance = axios.create({
     // baseURL: 'http://localhost:3001' // this is for the local host
-    baseURL: 'https://apple-clone-backend-luwq.onrender.com/api' 
+    baseURL: 'https://apple-clone-backend-luwq.onrender.com/iphones:1' 
 });
 
 export default instance;
