a7ee2f0aed641208e701f5426d79154e5e364b02
Natnael Tsega <42552538+natenaltsega2225@users.noreply.github.com>
2024-10-23 20:15:07 -0500

    updated axios file

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -1,8 +1,8 @@
-import axios from 'axios'
+import axios from 'axios';
 
 const instance = axios.create({
     // baseURL: 'http://localhost:3001' // this is for the local host
-    baseURL: 'https://apple-clone-backend-luwq.onrender.com/iphones:1' 
+    baseURL: 'https://apple-clone-backend-luwq.onrender.com'  // Production backend
 });
 
 export default instance;
